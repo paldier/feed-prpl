567d9fb6373974044e4662e2ba46f3a47fc2c761
Tomer Eliyahu <tomer.b.eliyahu@intel.com>
2019-11-24 08:46:42 -0800

    feed-prpl: Add prplmesh
    Add prplmesh to feed-prpl.
    
    Signed-off-by: Tomer Eliyahu <tomer.b.eliyahu@intel.com>

--- prplmesh/files/99-prplmesh-update-config.sh
+++ prplmesh/files/99-prplmesh-update-config.sh
@@ -1,8 +1,11 @@
 #!/bin/sh
 
 #for RX40 platform, wlan0 and wlan2 are used instead of the defualt wlan0 and wlan1
+#sta ifaces are wlan1 and wlan3
 if grep -q GRX /tmp/sysinfo/board_name; then
         uci set prplmesh.radio1.hostap_iface="wlan2"
         uci set prplmesh.radio1.hostap_iface_steer_vaps="wlan2.0"
+        uci set prplmesh.radio0.sta_iface="wlan1"
+        uci set prplmesh.radio1.sta_iface="wlan3"
         uci commit prplmesh
 fi
