f4004e6e51850748378d338001109013f4e88fda
Jo-Philipp Wich <jo@mein.io>
2020-06-03 22:28:25 +0200

    luci-mod-simple: complete network settings view
    Signed-off-by: Jo-Philipp Wich <jo@mein.io>

--- luci/luci-mod-simple/htdocs/luci-static/resources/view/simple/network.js
+++ luci/luci-mod-simple/htdocs/luci-static/resources/view/simple/network.js
@@ -1,4 +1,5 @@
 'use strict';
+'require validation';
 'require network';
 'require view';
 'require form';
@@ -56,12 +57,131 @@ function updateHostSection(mac, options) {
 			uci.set('dhcp', sid, opt, options[opt]);
 }
 
+function parseAddressAndNetmask(ipaddr, netmask) {
+	var m = (ipaddr || '').match(/^(.+)\/(\d+)$/);
+	if (m) {
+		var a = validation.parseIPv4(m[1]),
+		    s = network.prefixToMask(m[2]);
+
+		if (a && s)
+			return [ m[1], s ];
+	}
+	else {
+		m = (ipaddr || '').match(/^(.+)\/(.+)$/);
+
+		if (m) {
+			var a = validation.parseIPv4(m[1]),
+			    s = network.maskToPrefix(m[2]);
+
+			if (a && s)
+				return [ m[1], network.prefixToMask(s) ];
+		}
+		else {
+			return [ ipaddr, netmask ];
+		}
+	}
+
+	return null;
+}
+
+function calculateBroadcast(s, use_cfgvalue) {
+	var readfn = use_cfgvalue ? 'cfgvalue' : 'formvalue',
+	    addropt = s.children.filter(function(o) { return o.option == 'addr'})[0],
+	    maskopt = s.children.filter(function(o) { return o.option == 'mask'})[0];
+
+	var addr = validation.parseIPv4(L.toArray(addropt[readfn](s.section))[0] || ''),
+	    mask = L.toArray(maskopt[readfn](s.section))[0];
+
+	if (addr == null || mask == null)
+		return null;
+
+	if (!isNaN(mask))
+		mask = validation.parseIPv4(network.prefixToMask(+mask));
+	else
+		mask = validation.parseIPv4(mask);
+
+	var bc = [
+		addr[0] | (~mask[0] >>> 0 & 255),
+		addr[1] | (~mask[1] >>> 0 & 255),
+		addr[2] | (~mask[2] >>> 0 & 255),
+		addr[3] | (~mask[3] >>> 0 & 255)
+	];
+
+	return bc.join('.');
+}
+
+function validateAddressAndMask(topology, section, section_id, value) {
+	var broadcast = calculateBroadcast(section, false);
+
+	for (var zone in topology.zones) {
+		if (zone == section_id)
+			continue;
+
+		for (var device in topology.zones[zone]) {
+			var dev = topology.zones[zone][device];
+			if (Array.isArray(dev.ipaddrs)) {
+				for (var i = 0; i < dev.ipaddrs.length; i++) {
+					if (dev.ipaddrs[i].broadcast == broadcast)
+						return _('This address range is already used by the %s network').format(zone);
+				}
+			}
+		}
+	}
+
+	return true;
+}
+
+function validateNetmask(topology, section, section_id, value) {
+	var maskopt = section.children.filter(function(o) { return o.option == 'mask'})[0],
+	    maskval = maskopt.formvalue(section_id);
+
+	if (maskval == null || network.maskToPrefix(maskval) == null)
+		return _('Invalid network mask');
+
+	return validateAddressAndMask(topology, section, section_id, value);
+}
+
+function renderConnection(host, topo, wnets) {
+	var details = topo.hosts[host.mac] || {},
+	    wnet = details.assoc ? wnets.filter(function(w) { return w.ubus('dev', 'iwinfo', 'phy') == details.assoc.phy })[0] : null,
+	    port;
+
+	if (wnet) {
+		if (wnet.getChannel() > 13)
+			port = _('5GHz Wireless "%h"').format(wnet.getActiveSSID() || wnet.getActiveBSSID() || '?');
+		else
+			port = _('2.4GHz Wireless "%h"').format(wnet.getActiveSSID() || wnet.getActiveBSSID() || '?');
+	}
+	else if (details.assoc) {
+		port = _('Wireless');
+	}
+	else {
+		port = _('Ethernet');
+	}
+
+	switch (host.zone) {
+	case 'lan':
+		return _('LAN Network - %s').format(port);
+
+	case 'guest':
+		return _('Guest Network - %s').format(port);
+
+	case 'wan':
+		return _('WAN Network');
+
+	default:
+		return host.zone ? _('Network "%h"').format(host.zone) : '';
+	}
+}
+
 return view.extend({
 	loadStatus: function() {
 		return Promise.all([
 			L.resolveDefault(callTopologyGetTopology(), {}),
+			L.resolveDefault(network.getWifiNetworks(), []),
 			uci.load('dhcp'),
-			uci.load('firewall')
+			uci.load('firewall'),
+			uci.load('network')
 		]);
 	},
 
@@ -127,6 +247,7 @@ return view.extend({
 				return;
 
 			hosts[host.mac.toUpperCase()] = Object.assign({}, {
+				mac: host.mac.toUpperCase(),
 				config_ip: host.ip,
 				config_name: host.name,
 				config_type: host.type || 'laptop'
@@ -147,6 +268,13 @@ return view.extend({
 				else
 					ip6s.push(a);
 			}
+
+			(host.neigh4 || []).sort(function(a, b) {
+				var usedA = (a.used || '-1').split(/\//)[0],
+				    usedB = (b.used || '-1').split(/\//)[0];
+
+				return (usedA - usedB);
+			});
 
 			hosts[mac] = Object.assign(hosts[mac] || {}, {
 				mac: mac,
@@ -189,10 +317,10 @@ return view.extend({
 		return this.loadStatus();
 	},
 
-	renderHost: function(host) {
+	renderHost: function(host, topo, wnets) {
 		var table = E('div', { 'style': 'display:flex;flex-direction:column' });
 		var data = [
-			_('Connection:'),              '%s / %s'.format(host.connection, host.zone),
+			_('Connection:'),              renderConnection(host, topo, wnets),
 			_('IPv4 Address:'),            host.active_ip || host.config_ip || '',
 			_('IPv4 Address is from:'),    (host.active_ip || host.config_ip) ? ((host.active_ipmode == 'dhcp') ? 'DHCP' : _('Static', 'Statically configured IP, as opposed to DHCP assigned')) : '',
 			_('IPv6 Global:'),             L.toArray(host.active_ip6addrs).join(', '),
@@ -229,7 +357,7 @@ return view.extend({
 		o.value('phone',    E([], [ E('img', { 'style': 'width:1em', 'src': L.resource('svg/phone.svg') }),    ' ', _('Phone') ]));
 		o.value('router',   E([], [ E('img', { 'style': 'width:1em', 'src': L.resource('svg/router.svg') }),   ' ', _('Router') ]));
 
-		if (host.zone == 'lan' || host.zone == 'guest') {
+		if (host.zone == 'lan') {
 			o = s.option(form.Value, 'config_ip', _('Device Address'), _('Change the reserved DHCP address for this device. Has no effect on devices that use a static configuration and only takes effect when the device reconnects.'));
 			o.placeholder = host.active_ip;
 			o.datatype = 'ip4addr("nomask")';
@@ -331,8 +459,7 @@ return view.extend({
 		    info = [];
 
 		if (L.isObject(details.dhcp)) {
-			var ul = E('ul'),
-			    expires;
+			var expires;
 
 			if (details.dhcp.expires === false)
 				expires = E('em', [ _('unlimited') ]);
@@ -341,7 +468,7 @@ return view.extend({
 			else
 				expires = '%t'.format(details.dhcp.expires);
 
-			info.push(E('h6', [ _('DHCP') ]), ui.itemlist(E('div'), [
+			info.push(E('h6', [ _('DHCP') ]), ui.itemlist(E('p'), [
 				_('Hostname'), details.dhcp.hostname || E('em', [ _('not sent') ]),
 				_('Address'),  details.dhcp.ipaddr,
 				_('Expires'),  expires
@@ -349,8 +476,7 @@ return view.extend({
 		}
 
 		if (L.isObject(details.dhcp6)) {
-			var ul = E('ul'),
-			    expires;
+			var expires;
 
 			if (details.dhcp6.expires === false)
 				expires = E('em', [ _('unlimited') ]);
@@ -359,7 +485,7 @@ return view.extend({
 			else
 				expires = '%t'.format(details.dhcp6.expires);
 
-			info.push(E('h6', [ _('DHCP') ]), ui.itemlist(E('div'), [
+			info.push(E('h6', [ _('DHCP') ]), ui.itemlist(E('p'), [
 				_('Hostname'), details.dhcp6.hostname || E('em', [ _('not sent') ]),
 				_('Address'),  details.dhcp6.ipaddr,
 				_('Expires'),  expires,
@@ -404,6 +530,9 @@ return view.extend({
 			info.push(E('h6', [ _('NDP') ]), ul);
 		}
 
+		/* TODO: render wireless details */
+		/* info.push(E('pre', JSON.stringify(details, null, '  '))); */
+
 		ui.showModal(_('Details for %s').format(host.config_name || host.active_name || host.mac), [
 			E('div', info),
 			E('div', { 'class': 'right' }, [
@@ -435,7 +564,8 @@ return view.extend({
 		return uci.save().then(function() {
 			return Promise.all([
 				callUciCommit('dhcp'),
-				callUciCommit('firewall')
+				callUciCommit('firewall'),
+				callUciCommit('network')
 			]);
 		}).then(L.bind(this.redraw, this)).catch(function(err) {
 			ui.addNotification(null, [ E('p', [ _('Failed to save configuration: %s').format(err) ]) ])
@@ -487,6 +617,59 @@ return view.extend({
 		return E('div', { 'style': 'width:200px' }, dd.render());
 	},
 
+	renderHostListSelector: function() {
+		var choices = {
+			all: _('Display all devices'),
+			active: _('Show active devices'),
+			blocked: _('Show blocked devices'),
+			lan: _('Show LAN devices'),
+			guest: _('Show guest devices')
+		};
+
+		var dd = new ui.Dropdown(null, choices, {
+			sort: [ 'all', 'active', 'blocked', 'lan', 'guest' ],
+			optional: false
+		});
+
+		dd.toggleItem = function(sb, li) {
+			var table = document.querySelector('div[data-tab="hosts"] > .table'),
+			    rows = table.querySelectorAll('.tr:not(.placeholder)'),
+			    mode = li.getAttribute('data-value'),
+			    matched = 0;
+
+			rows.forEach(function(row) {
+				switch (mode) {
+				case 'all':
+					row.style.display = '';
+					matched++;
+					return;
+
+				case 'active':
+					row.style.display = (row.getAttribute('data-connected') == 'true') ? '' : 'none';
+					matched += (row.style.display != 'none');
+					return;
+
+				case 'blocked':
+					row.style.display = (row.getAttribute('data-blocked') == 'true') ? '' : 'none';
+					matched += (row.style.display != 'none');
+					return;
+
+				case 'lan':
+				case 'guest':
+					row.style.display = (row.getAttribute('data-zone') == mode) ? '' : 'none';
+					matched += (row.style.display != 'none');
+					return;
+				}
+			});
+
+			table.firstElementChild.style.display = matched ? 'none' : '';
+
+			ui.Dropdown.prototype.toggleItem.apply(this, [sb, li]);
+		};
+
+		return E('p', { 'class': 'right' }, dd.render());
+	},
+
 	renderHostIcon: function(host) {
 		return E('img', {
 			'src': L.resource('svg/%s.svg'.format(host.config_type || 'laptop')),
@@ -496,12 +679,23 @@ return view.extend({
 
 	renderHostList: function(data) {
 		var topo = data[0],
+		    wnets = data[1],
 		    list = this.buildHostList(topo);
 
-		var view = E('div', { 'class': 'table' });
+		var view = E('div', { 'class': 'table' }, [
+			E('div', { 'class': 'tr placeholder', 'style': list.length ? 'display:none' : '' }, [
+				E('div', { 'class': 'td' }, [
+					E('em', [ _('No devices found') ])
+				])
+			])
+		]);
 
 		for (var i = 0; i < list.length; i++)
-			view.appendChild(E('div', { 'class': 'tr' }, [
+			view.appendChild(E('div', {
+				'class': 'tr', 'data-zone': list[i].zone,
+				'data-connected': list[i].connected,
+				'data-blocked': list[i].blocked
+			}, [
 				E('div', { 'class': 'td' }, [
 					E('div', { 'style': 'display:flex' }, [
 						E('div', { 'style': 'flex:0 0 50px' }, this.renderHostIcon(list[i])),
@@ -510,7 +704,7 @@ return view.extend({
 								E('strong', { 'style': 'flex:1 1 40%' }, [ list[i].config_name || list[i].active_name || 'unknown_%s'.format(list[i].mac) ]),
 								E('div', { 'style': 'flex:1 1 60%' }, this.renderHostAction(list[i], topo))
 							]),
-							this.renderHost(list[i])
+							this.renderHost(list[i], topo, wnets)
 						])
 					])
 				])
@@ -519,17 +713,134 @@ return view.extend({
 		return view;
 	},
 
+	handleSettingsSave: function(formdata) {
+		var m = L.dom.findClassInstance(document.querySelector('div[data-tab="settings"] > .cbi-map'));
+
+		return m.save().then(L.bind(function() {
+			for (var net = 'lan'; net != null; net = (net == 'lan') ? 'guest' : null) {
+				uci.set('network', net, 'ipaddr', formdata[net].addr);
+				uci.set('network', net, 'netmask', formdata[net].mask);
+
+				if (formdata[net].dhcp != '1')
+					uci.set('dhcp', net, 'ignore', '1');
+				else
+					uci.set('dhcp', net, 'ignore', '0');
+
+				if (formdata[net].ipv6 != '1') {
+					uci.set('dhcp', net, 'ra', 'disabled');
+					uci.set('dhcp', net, 'dhcpv6', 'disabled');
+				}
+				else {
+					uci.set('dhcp', net, 'ra', 'server');
+					uci.set('dhcp', net, 'dhcpv6', 'server');
+				}
+			}
+
+			return this.handleApply();
+		}, this));
+	},
+
+	renderNetworkSettings: function(data, formdata) {
+		var m, s, o;
+
+		var addr_lan = parseAddressAndNetmask(
+			uci.get('network', 'lan', 'ipaddr'),
+			uci.get('network', 'lan', 'netmask'));
+
+		var addr_guest = parseAddressAndNetmask(
+			uci.get('network', 'guest', 'ipaddr'),
+			uci.get('network', 'guest', 'netmask'));
+
+		Object.assign(formdata, {
+			lan: {
+				addr: addr_lan ? addr_lan[0] : null,
+				mask: addr_lan ? addr_lan[1] : null,
+				dhcp: (uci.get('dhcp', 'lan', 'ignore') != '1') ? '1' : '0',
+				ipv6: (uci.get('dhcp', 'lan', 'dhcpv6') != 'disabled') ? '1' : '0'
+			},
+			guest: {
+				addr: addr_guest ? addr_guest[0] : null,
+				mask: addr_guest ? addr_guest[1] : null,
+				dhcp: (uci.get('dhcp', 'guest', 'ignore') != '1') ? '1' : '0',
+				ipv6: (uci.get('dhcp', 'guest', 'dhcpv6') != 'disabled') ? '1' : '0'
+			}
+		});
+
+		m = new form.JSONMap(formdata);
+
+		s = m.section(form.NamedSection, 'lan', 'lan', _('Local Network Settings'));
+
+		o = s.option(form.Value, 'addr', _('IP Address'));
+		o.rmempty = false;
+		o.datatype = 'ip4addr("nomask")';
+		o.validate = validateAddressAndMask.bind(o, data[0], s);
+
+		o = s.option(form.Value, 'mask', _('Netmask'));
+		o.rmempty = false;
+		o.datatype = 'ip4addr("nomask")';
+		o.validate = validateNetmask.bind(o, data[0], s);
+
+		o = s.option(form.Flag, 'dhcp', _('Enable DHCP Service'));
+		o = s.option(form.Flag, 'ipv6', _('Enable IPv6'));
+
+
+		s = m.section(form.NamedSection, 'guest', 'guest', _('Guest Network Settings'));
+
+		o = s.option(form.Value, 'addr', _('IP Address'));
+		o.rmempty = false;
+		o.datatype = 'ip4addr("nomask")';
+		o.validate = validateAddressAndMask.bind(o, data[0], s);
+
+		o = s.option(form.Value, 'mask', _('Netmask'));
+		o.rmempty = false;
+		o.datatype = 'ip4addr("nomask")';
+		o.validate = validateNetmask.bind(o, data[0], s);
+
+		o = s.option(form.Flag, 'dhcp', _('Enable DHCP Service'));
+		o = s.option(form.Flag, 'ipv6', _('Enable IPv6'));
+
+		return m.render();
+	},
+
 	redraw: function() {
 		this.load().then(L.bind(function(data) {
-			dom.content(
-				document.querySelector('#view'),
-				this.renderHostList(data
-			));
+			var oldTable = document.querySelector('div[data-tab="hosts"] > .table'),
+			    newTable = this.renderHostList(data),
+			    filter = document.querySelector('div[data-tab="hosts"] > p > .cbi-dropdown')
+
+			oldTable.parentNode.replaceChild(newTable, oldTable);
+			L.dom.callClassMethod(filter, 'toggleItem', filter, filter.querySelector('li[selected]'));
 		}, this));
 	},
 
 	render: function(data) {
-		return this.renderHostList(data);
+		var formdata = {};
+
+		return Promise.all([
+			this.renderHostList(data),
+			this.renderNetworkSettings(data, formdata)
+		]).then(L.bind(function(panes) {
+			var view = E('div', [
+				E('div', [
+					E('div', { 'data-tab': 'hosts', 'data-tab-title': _('Connected Devices') }, [
+						this.renderHostListSelector(),
+						panes[0]
+					]),
+					E('div', { 'data-tab': 'settings', 'data-tab-title': _('Network Settings') }, [
+						panes[1],
+						E('div', { 'class': 'right' }, [
+							E('button', {
+								'click': ui.createHandlerFn(this, 'handleSettingsSave', formdata)
+							}, [ _('Save settings') ])
+						])
+					])
+				])
+			]);
+
+			ui.tabs.initTabGroup(view.firstElementChild.childNodes);
+
+			return view;
+		}, this));
 	},
 
 	handleSave: null,
