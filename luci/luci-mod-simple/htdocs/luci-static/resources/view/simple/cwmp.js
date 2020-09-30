d8ffe87ad72582966ec60833d6bcdb1ef36dad39
ZhaoWei Xu <paldier@hotmail.com>
2020-09-30 14:38:30 +0800

    merging from upstream

--- luci/luci-mod-simple/htdocs/luci-static/resources/view/simple/cwmp.js
+++ luci/luci-mod-simple/htdocs/luci-static/resources/view/simple/cwmp.js
@@ -5,27 +5,73 @@
 
 return view.extend({
 	render: function() {
-		var m, s, o;
+		var m, s, o, t;
 
-		m = new form.Map('cwmp');
+		m = new form.Map('cwmp', 'CWMP / TR-069');
 
-		s = m.section(form.NamedSection, 'acs', _('ACS'));
+		s = m.section(form.NamedSection, 'cwmp', _('Enable'));
 		s.anonymous   = false;
 		s.addremove   = false;
 		s.addbtntitle = _('ACS settings');
 
-		o = s.option(form.Flag, 'enable', _('Enable'));
-		o.enabled = 'true';
-		o.disabled = 'false'
+		o = s.option(form.Flag, 'enabled', _('Enable'));
+		o.enabled = '1';
+		o.disabled = '0'
+
+		s = m.section(form.NamedSection, 'acs', _('ACS'), 'ACS Settings');
+		s.anonymous   = false;
+		s.addremove   = false;
+		s.addbtntitle = _('ACS settings');
 
 		o = s.option(form.Value, 'url', _('URL'));
-		o.depends('enable', 'true');
+		o.depends('cwmp.cwmp.enabled', '1');
 
 		o = s.option(form.Value, 'username', _('Username'));
-		o.depends('enable', 'true');
+		o.depends('cwmp.cwmp.enabled', '1');
 
 		o = s.option(form.Value, 'password', _('Password'));
-		o.depends('enable', 'true');
+		o.depends('cwmp.cwmp.enabled', '1');
+
+		o = s.option(form.Flag, 'periodic_inform_enable', _('Periodic Inform'));
+		o.enabled  = '1';
+		o.disabled = '0';
+		o.depends('cwmp.cwmp.enabled', '1');
+
+		o = s.option(form.Value, 'periodic_inform_interval', _('Periodic Inform Interval'));
+		o.depends({ 'cwmp.cwmp.enabled': '1', 'periodic_inform_enable': '1' });
+
+		o = s.option(form.Value, 'periodic_inform_time', _('Periodic Inform Time'));
+		o.depends({ 'cwmp.cwmp.enabled': '1', 'periodic_inform_enable': '1' });
+
+		o = s.option(form.Flag, 'dhcp_discovery', _('DHCP Discovery'));
+		o.enabled  = 'enable';
+		o.disabled = 'disable';
+		o.depends('cwmp.cwmp.enabled', '1');
+
+		o = s.option(form.Value, 'ParameterKey', _('Parameter Key'));
+		o.depends('cwmp.cwmp.enabled', '1');
+
+		s = m.section(form.NamedSection, 'cpe', _('CPE'), 'CPE Settings');
+		s.anonymous   = false;
+		s.addremove   = false;
+		s.addbtntitle = _('CPE settings');
+
+		o = s.option(form.Value, 'port', _('Port'));
+		o.depends('cwmp.cwmp.enabled', '1');
+
+		o = s.option(form.Value, 'userid', _('Username'));
+		o.depends('cwmp.cwmp.enabled', '1');
+
+		o = s.option(form.Value, 'passwd', _('Password'));
+		o.depends('cwmp.cwmp.enabled', '1');
+
+		o = s.option(form.Value, 'provisioning_code', _('Provisioning Code'));
+		o.depends('cwmp.cwmp.enabled', '1');
+
+		o = s.option(form.Flag, 'dhcp_discovery', _('Managed Upgrades'));
+		o.enabled  = 'true';
+		o.disabled = 'false';
+		o.depends('cwmp.cwmp.enabled', '1');
 
 		return m.render();
 	}
--- luci/luci-mod-simple/htdocs/luci-static/resources/view/simple/usp.js
+++ luci/luci-mod-simple/htdocs/luci-static/resources/view/simple/usp.js
@@ -7,10 +7,10 @@ return view.extend({
 	render: function() {
 		var m, s, o;
 
-		m = new form.Map('obuspa');
+		m = new form.Map('obuspa', _('USP / TR-369'));
 
-		s = m.section(form.TypedSection, 'controller', _('USP'));
-		s.anonymous   = true;
+		s = m.section(form.NamedSection, 'mtp', _('Enable'));
+		s.anonymous   = false;
 		s.addremove   = false;
 		s.addbtntitle = _('USP settings');
 
@@ -18,19 +18,24 @@ return view.extend({
 		o.enabled = 'true';
 		o.disabled = 'false'
 
+		s = m.section(form.NamedSection, 'connection', _('Connection'), _('Connection Settings'));
+		s.anonymous   = true;
+		s.addremove   = false;
+		s.addbtntitle = _('USP settings');
+
 		o = s.option(form.Value, 'endpoint', _('Endpoint'));
-		o.depends('enable', 'true');
+		o.depends('obuspa.mtp.enable', 'true');
 		o.default = 'self::example.controller.com';
 
 		o = s.option(form.Value, 'host', _('Host'));
-		o.depends('enable', 'true');
+		o.depends('obuspa.mtp.enable', 'true');
 		o.default = 'example.controller.com';
 
 		o = s.option(form.Value, 'username', _('Username'));
-		o.depends('enable', 'true');
+		o.depends('obuspa.mtp.enable', 'true');
 
 		o = s.option(form.Value, 'password', _('Password'));
-		o.depends('enable', 'true');
+		o.depends('obuspa.mtp.enable', 'true');
 
 		return m.render();
 	}
